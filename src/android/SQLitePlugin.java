2055898dbfd0819f607caf0deea69478434865e0
Nolan Lawson <nolan.lawson@gmail.com>
2016-03-24 13:35:35 -0400

    make progress

--- src/android/SQLitePlugin.java
+++ src/android/SQLitePlugin.java
@@ -9,21 +9,21 @@ import org.json.JSONObject;
 
 public class SQLitePlugin extends CordovaPlugin {
 
-@Override
-public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     if (action.equals("echo")) {
-        String message = args.getString(0);
-        this.echo(message, callbackContext);
-        return true;
+      String message = args.getString(0);
+      this.echo(message, callbackContext);
+      return true;
     }
     return false;
-}
+  }
 
-private void echo(String message, CallbackContext callbackContext) {
+  private void echo(String message, CallbackContext callbackContext) {
     if (message != null && message.length() > 0) {
-        callbackContext.success(message);
+      callbackContext.success(message);
     } else {
-        callbackContext.error("Expected one non-empty string argument.");
+      callbackContext.error("Expected one non-empty string argument.");
     }
-}
+  }
 }
