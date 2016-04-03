1e2e21e035be06787d1e41219025c39402f72f3f
Nolan Lawson <nolan.lawson@gmail.com>
2016-04-03 19:05:41 -0400

    put tests back in

--- test/test.js
+++ test/test.js
@@ -6,12 +6,12 @@ describe('sqlite plugin test suite', function () {
   this.timeout(180000);
 
   require('./test.main.js');
-  //require('./test.compaction.js');
-  //require('./test.mapreduce.js');
-  //require('./test.attachments.js');
-  //require('./test.basics.js');
-  //require('./test.changes.js');
-  //require('./test.bulk_docs.js');
-  //require('./test.all_docs.js');
-  //require('./test.replication.js');
+  require('./test.compaction.js');
+  require('./test.mapreduce.js');
+  require('./test.attachments.js');
+  require('./test.basics.js');
+  require('./test.changes.js');
+  require('./test.bulk_docs.js');
+  require('./test.all_docs.js');
+  require('./test.replication.js');
 });
