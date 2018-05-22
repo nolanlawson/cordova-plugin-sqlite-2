9b9831a1597b6ec2840ddaca59a79ce59c94e0ac
Nolan Lawson <nolan@nolanlawson.com>
2018-05-21 21:46:18 -0700

    fix android install

--- bin/install-android-on-travis.sh
+++ bin/install-android-on-travis.sh
@@ -4,6 +4,7 @@ if [[ $PLATFORM != 'android' ]]; then
   exit 0
 fi
 
-brew install android
+brew tap caskroom/cask
+brew cask install android-sdk
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | /usr/local/bin/android update \
   sdk --no-ui --all --filter platform-tool,android-23,build-tools-23.0.3
