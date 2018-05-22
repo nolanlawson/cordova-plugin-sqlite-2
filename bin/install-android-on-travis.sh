da6aefa03646b38f83cdee41c6c0099f7f32e252
Nolan Lawson <nolan@nolanlawson.com>
2018-05-22 08:42:27 -0700

    move gradle to android installer

--- bin/install-android-on-travis.sh
+++ bin/install-android-on-travis.sh
@@ -8,3 +8,5 @@ brew tap caskroom/cask
 brew cask install android-sdk
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | /usr/local/bin/sdkmanager \
   "platform-tools" "platforms;android-23" "build-tools;26.0.3"
+
+brew install gradle
