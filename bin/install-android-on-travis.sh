e87ae0107804d8f88ea51e12287780a691f41665
Nolan Lawson <nolan@nolanlawson.com>
2018-05-21 23:28:58 -0700

    try updating android build tools

--- bin/install-android-on-travis.sh
+++ bin/install-android-on-travis.sh
@@ -7,4 +7,4 @@ fi
 brew tap caskroom/cask
 brew cask install android-sdk
 ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | /usr/local/bin/sdkmanager \
-  "platform-tools" "platforms;android-23" "build-tools;23.0.3"
+  "platform-tools" "platforms;android-23" "build-tools;26.0.3"
