160553d7ca8b3cb8566299006f09e595dc792311
Nolan Lawson <nolan@nolanlawson.com>
2018-05-21 22:30:22 -0700

    use new android install method

--- bin/install-android-on-travis.sh
+++ bin/install-android-on-travis.sh
@@ -6,5 +6,5 @@ fi
 
 brew tap caskroom/cask
 brew cask install android-sdk
-( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | /usr/local/bin/android update \
-  sdk --no-ui --all --filter platform-tool,android-23,build-tools-23.0.3
+( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | /usr/local/bin/sdkmanager \
+  "platform-tools" "platforms;android-23" "build-tools;23.0.3"
